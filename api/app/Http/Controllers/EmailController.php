f8459157bffa87dd3d79e8cb0f13bb97a7bed00c
admin <admin@admins-MacBook-Pro.local>
2021-04-21 13:10:18 +0100

    Created api routes and functions in email controller

--- api/app/Http/Controllers/EmailController.php
+++ api/app/Http/Controllers/EmailController.php
@@ -6,5 +6,17 @@ use Illuminate\Http\Request;
 
 class EmailController extends Controller
 {
-    //
+    public function store(){
+
+    }
+    public function search(){
+
+    }
+    public function getById(){
+
+    }
+    public function getByRecipient(){
+
+    }
+
 }
--- api/routes/api.php
+++ api/routes/api.php
@@ -1,7 +1,7 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\EmailController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +14,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+
+Route::post('/email/store',[EmailController::class,'store']);
+Route::get('/email/{id}',[EmailController::class,'getById']);
+Route::get('/email/recipient/{recipient}',[EmailController::class,'getByRecipient']);
+Route::get('/email/search',[EmailController::class,'search']);
+
+
