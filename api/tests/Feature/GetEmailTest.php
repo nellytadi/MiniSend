c0e2a3abe8c590e97862114e0eede8bfb2c38e31
admin <admin@admins-MacBook-Pro.local>
2021-04-21 21:18:23 +0100

    Created test to getEmailById

--- api/tests/Feature/GetEmailTest.php
+++ api/tests/Feature/GetEmailTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature;
 
+use App\Models\Email;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
@@ -13,10 +14,19 @@ class GetEmailTest extends TestCase
      *
      * @return void
      */
-    public function test_example()
+    public function testGetEmailById()
     {
-        $response = $this->get('/');
+        Email::factory(5)->create();
 
-        $response->assertStatus(200);
+        $emails = Email::inRandomOrder()->limit(2)->get()->toArray();
+        foreach($emails as $email){
+            $response = $this->get('/api/email/'.$email['id']);
+            $response->assertStatus(200)->assertJson($email);
+        }
+
     }
+    public function testGetEmailByIdReturnsEmptyIfEmailDoesNotExist(){
+
+    }
+
 }
