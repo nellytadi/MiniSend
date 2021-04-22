b5c41d329ff3363385b3bdb14aa84c2f74449419
admin <admin@admins-MacBook-Pro.local>
2021-04-22 14:22:25 +0100

    wrote test to search email using subject parameter

--- api/tests/Feature/SearchEmailTest.php
+++ api/tests/Feature/SearchEmailTest.php
@@ -11,7 +11,7 @@ use Illuminate\Testing\Fluent\AssertableJson;
 
 class SearchEmailTest extends TestCase
 {
-  //  use WithFaker;
+    use WithFaker;
 
     public function testSearchWithFromParameter(){
         $from = Str::random(10).'@mailsender.com';
@@ -63,6 +63,26 @@ class SearchEmailTest extends TestCase
     }
 
     public function testSearchWithSubjectParameter(){
+        $subject = $this->faker->sentence;
+        $count = 5;
+        Email::factory($count)->state([
+            'subject' => $subject
+        ])->create();
+
+        $response = $this->json('GET','/api/email/search?subject='.$subject);
+
+        //see it returns 5 record all 'to' is the $to created
+        $response
+            ->assertStatus(200)
+            ->assertJson(fn (AssertableJson $json) =>
+            $json->has('data', $count)
+                ->has('data', $count, fn ($json) =>
+                $json
+                    ->where('subject', $subject)
+                    ->etc()
+                )
+            );
+
 
     }
 //    public function testSearchWithStatusParameters(){
